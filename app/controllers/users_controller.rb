e527d1f2d0208b37b9419f7f133d0f59b8b5c06b
satoru-koyama <satoru123.fly.9.26@gmail.com>
2021-06-22 08:39:50 +0000

    [Update] /Bookers2/app/controllers/users_controller.rb, /Bookers2/app/views/users/edit.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    flash[:user_update] = "You have updated user successfully."
-    redirect_to user_path(user)
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:user_update] = "You have updated user successfully."
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
